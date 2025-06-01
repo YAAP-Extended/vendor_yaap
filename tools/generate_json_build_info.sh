2e4aeb160ff8a86e09db62ef314961b00f29f224
Ido Ben-Hur <idoybh2@gmail.com>
2025-06-01 15:19:30 +0300

    generate_json_build_info.sh: Fixup late device_code init

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -37,6 +37,7 @@ fi
 file_path=$1
 file_dir=$(dirname "$file_path")
 file_name=$(basename "$file_path")
+device_code=$(echo "${file_name}" | cut -d'-' -f5)
 
 if ! [ -f "$file_path" ]; then
     echo -e "${RED}File does not exist${NC}"
@@ -101,6 +102,5 @@ fi
         echo "}"
 } >> "${file_path}.json"
 
-device_code=$(echo "${file_name}" | cut -d'-' -f5)
 mv "${file_path}.json" "${file_dir}/${device_code}.json"
 echo -e "${GREEN}Done generating ${YELLOW}${file_dir}/${device_code}.json${NC}"
