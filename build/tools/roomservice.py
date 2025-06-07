001ee7eeb931d63f07438a97dcb10c78fa6540b6
Guidix <guidixm@gmail.com>
2025-06-07 23:45:24 +0300

    build: fixup syncing dependencies

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -34,8 +34,8 @@ default_manifest = ".repo/manifest.xml"
 custom_local_manifest = ".repo/local_manifests/yaap_manifest.xml"
 custom_default_revision = "fifteen"
 custom_dependencies = "yaap.dependencies"
-org_manifest = "yaap"  # leave empty if org is provided in manifest
-org_display = "YAAP"  # needed for displaying
+org_manifest = "yaap-extended"  # leave empty if org is provided in manifest
+org_display = "YAAP-Extended"  # needed for displaying
 
 github_auth = None
 
